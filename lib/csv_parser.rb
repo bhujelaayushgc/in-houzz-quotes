95002588e1170399866b1e32ed657365c78b698f
bhujelaayushgc <bhujel.aayushgc@gmail.com>
2022-08-26 17:35:51 +0545

    chore: Removed comments

--- lib/csv_parser.rb
+++ lib/csv_parser.rb
@@ -5,9 +5,6 @@ module CsvParser
 
   def self.sample_quotes
     QUOTES_ARR.sample.to_h
-    # total_rows = CSV.foreach("#{Rails.root}/quotes.csv", headers: true).count
-    # random = rand(total_rows)
-    # { status: :ok, message: random }
   end
   
 end
